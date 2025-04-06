7dfeca4d395f2d610dbb00ae50aceaab1905b4b1
Meghna Reddy <meghna.reddy3005@gmail.com>
2025-04-05 23:54:42 -0400

    first attempt

--- js/video.js
+++ js/video.js
@@ -2,10 +2,76 @@ var video;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
+	video = this.document.querySelector("video")
+	document.querySelector("#volume").innerText = "100%";
+	video.volume = 1.0;
+
+	video.autoplay = false;
+	video.loop = false;
 
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+// Play
+document.querySelector("#play").addEventListener("click", function() {
+	video.play()
+	console.log("Play Video");
+	document.querySelector("#volume").innerText = "100%";
+});
 
+// Pause
+document.querySelector("#pause").addEventListener("click", function() {
+	video.pause()
+	console.log("Pause Video");
+});
+
+// Slow down 
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.9;
+	console.log("New slowed speed: " + video.playbackRate);
+})
+
+// Speed up
+document.querySelector("#faster").addEventListener("click", function() {
+	// video.playbackRate = video.playbackRate * 1.1;
+	video.playbackRate = video.playbackRate / 0.9;
+	console.log("New faster speed: " + video.playbackRate);
+});
+
+// Skip ahead
+document.querySelector("#skip").addEventListener("click", function() {
+	if (video.currentTime + 10 >= video.duration) {
+		video.currentTime = 0;
+	}
+	else {
+		video.currentTime = video.currentTime + 10;
+	}
+	console.log("New time: " + video.currentTime);
+});
+
+// Mute
+document.querySelector("#mute").addEventListener("click", function() {
+	video.muted = !video.muted
+	if (video.muted) {
+		this.innerText = "Unmute";
+	}
+	else {
+		this.innerText = "Mute";
+	}
+});
+
+// Volume slider
+document.querySelector("#slider").addEventListener("change", function() {
+	video.volume = this.value/100;
+	document.querySelector("#volume").innerText = this.value + "%";
+	console.log("Current volume: " + this.value/100);
+});
+
+// Styled
+document.querySelector("#vintage").addEventListener("click", function() {
+	video.classList.add("oldSchool");
+});
+
+// Original
+document.querySelector("#orig").addEventListener("click", function() {
+	video.classList.remove("oldSchool");
+});
